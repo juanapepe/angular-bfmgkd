73d88d20e949b0ea45817450f5193760d6bbe69f
juanapepe <jana.silhava@gmail.com>
2020-04-12 17:05:11 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {of, from} from 'rxjs';
+import {map, tap, take} from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -32,7 +33,34 @@ export class AppComponent implements OnInit  {
     of(...apples).subscribe( //takto to funguje jako s from
       item => console.log(`resulting item .. ${item}`),
       err => console.error(`error occured ${err}`),
+      () => console.log('complete s')
+    );
+
+    of(2 , 4 , 6)
+    .pipe(
+        map(item => item * 2),
+        tap(item => console.log(item)),
+        take(2)
+    ).subscribe(console.log);
+
+     from(apples)
+      .pipe(
+        map(item => item * 2),
+        map(item => item - 10),
+        map(item => {
+          if (item === 0) {
+            throw new Error('zero detected');
+          }
+          return item;
+        }),
+        take(3)
+      )
+      .subscribe(
+      item => console.log(`resulting item .. ${item}`),
+      err => console.error(`error occured ${err}`),
       () => console.log('complete')
     );
+
+
   }
 }
